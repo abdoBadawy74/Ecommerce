2c917c8362691eea1fb948368740714af9c8260e
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-08-27 11:46:16 +0300

    add theme code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import ToggleBar from "./Components/header/ToggleBar";
+import TopBar from "./Components/header/TopBar";
+import SearchBar from "./Components/header/SearchBar";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
 function App() {
-  return <></>;
+  const [theme, colorMode] = useMode();
+  return (
+    <>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <TopBar />
+          <SearchBar />
+          <ToggleBar />
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/header/ToggleBar.jsx
@@ -0,0 +1,3 @@
+export default function ToggleBar() {
+  return <div>ToggleBar</div>;
+}
